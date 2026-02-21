f1485511bf8284995aa3ac02b28f6e801521888f
dukektmdhanraj-a11y <dukektmdhanraj@gmail.com>
2026-02-21 19:56:52 +0530

    Update renderer.py

--- python/renderer.py
+++ python/renderer.py
@@ -5,7 +5,7 @@ def render_docx(template_path, filename, values):
     doc = DocxTemplate(template_path)
     doc.render(values)
 
-    output_path = os.path.join("/tmp", filename)  # save only in /tmp on Render
+    output_path = os.path.join("/tmp", filename)
     doc.save(output_path)
 
     return output_path
