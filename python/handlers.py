84362a93846f95864fc89cb8b01a158006713d48
dukektmdhanraj-a11y <dukektmdhanraj@gmail.com>
2026-02-22 09:17:22 +0530

    Update handlers.py

--- python/handlers.py
+++ python/handlers.py
@@ -1,10 +1,10 @@
-from python.renderer import render_docx
-
 def gift_handler(data, template_path):
     return data
 
+
 def sale_handler(data, template_path):
     return data
+
 
 def ots_handler(data, template_path):
     return data
